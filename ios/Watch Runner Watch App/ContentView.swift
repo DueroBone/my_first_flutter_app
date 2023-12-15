95f50885a82902c898b796e3ed3b8932de934803
Duel M <dueromoods@gmail.com>
2023-12-15 08:58:44 -0500

    Initial commit

---
+++ ios/Watch Runner Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Watch Runner Watch App
+//
+//  Created by Duel Mood on 12/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/Watch Runner Watch App/Watch_RunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_RunnerApp.swift
+//  Watch Runner Watch App
+//
+//  Created by Duel Mood on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Watch_Runner_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
